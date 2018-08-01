10b221bd42f6eceb7cb5b52cbac82c9937ddbde4
Brad Thompson <gbradthompson@gmail.com>
2018-07-31 22:24:41 -0600

    get data user from reddit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const axios = require('axios')
 const bodyParser = require('body-parser')
 const express = require('express')
-const credentials = require('./credentials.json')
+const reddit = require('./reddit')
 
 var app = express()
 
@@ -19,36 +18,39 @@ app.use((req, res, next) => {
 });
 
 
-app.post('/', (req, res) => {
-
-  authorizationToAccessToken(req, res).then(token => {
+app.post('/', async (req, res) => {
+  try {
+    let token = await reddit.authorizationCodeToAccessToken(req)
+    let user = await reddit.accessTokenToUser(token)
+    let subscriptions = await reddit.accessTokenToUserSubscriptions(token)
+    console.log(token)
+    console.log(user)
+    console.log(subscriptions)
     /*
-    { access_token: '63001037523-3Ez0IuXAh6fO_HP9H4r9ppWvM1k',
+    token = {
+      access_token: '63001037523-3Ez0IuXAh6fO_HP9H4r9ppWvM1k',
       token_type: 'bearer',
       expires_in: 3600,
-      scope: 'mysubreddits' }
+      scope: 'identity mysubreddits'
+    }
+    user = {
+      name: 'Alasa-Lerin',
+      id: 'sad8f6'
+    }
+    subscriptions = [{
+      name: 'linuxhardware',
+      id: '3gxbf',
+      favorited: false,
+      contributor: false
+    }]
     */
-  }).catch(err => {
-    res.status(err.code).send(err)
-  })
+
+    res.sendStatus(200)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(400)
+  }
 })
 
 app.listen(3412)
 console.log('listening on port 3412')
-
-async function authorizationToAccessToken (req, res) {
-  try {
-    if (req.body.code && req.body.state && typeof req.body.code === 'string' && req.body.code.length < 1000) {
-      let response = await axios.post(
-        'https://www.reddit.com/api/v1/access_token',
-        `grant_type=authorization_code&code=${req.body.code}&redirect_uri=http://localhost:3000/signup/`,
-        {headers: {authorization: 'Basic ' + Buffer.from(credentials.app_id + ':' + credentials.secret).toString('base64')}}
-      )
-      if (!response.data.error) return response.data
-      else throw {error: response.data.error, code: 400}
-    } else throw {error: 'malformed query string', code: 400}
-  } catch (err) {
-    if (!err.error) throw {error: 'unknown error', code: 418, err}
-    else throw err
-  }
-}
